ecf3c7b115e0a8a8feac576ff4f77b0e5a70a3fd
jeancy001 <jeancympoy24@gmail.com>
2025-10-12 16:53:55 +0100

    added  comming  event

--- src/app/HomePage.tsx
+++ src/app/HomePage.tsx
@@ -1,7 +1,16 @@
 
 function HomePage() {
   return (
-    <div> Home </div>
+    <div className="flex justify-center items-center  mt-5">
+
+
+<div className="flex items-center gap-2">
+  <div className="w-10 h-10 border-2 border-t-transparent border-blue-500 rounded-full animate-spin"></div>
+  <span className="text-blue-600 font-medium text-2xl ">Working on it...</span>
+</div>
+
+
+    </div>
   )
 }
 
