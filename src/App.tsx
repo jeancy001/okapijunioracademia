826742bf87af4e6aeaf5850e6be46fcd65fcfb1e
jeancy001 <jeancympoy24@gmail.com>
2025-12-02 07:46:28 +0100

    added

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,6 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./app/HomePage";
 import NavPage from "./app/NavPage";
-import MyClass from "./components/MyClass";
-import Login from "./components/Login";
-import ProtectedRoute from "./components/ProtectedRoute";
-import Register from "./components/Register";
 import Footer from "./app/Footer";
 
 function App() {
@@ -14,16 +10,6 @@ function App() {
       <NavPage />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route
-          path="/myClass"
-          element={
-            <ProtectedRoute>
-              <MyClass />
-            </ProtectedRoute>
-          }
-        />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
       </Routes>
 
       <Footer/>
